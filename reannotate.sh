4d955d4940e8a214c9e1c86dd8b7bb0af0ba3ef8
Catalin Ilea <catalin.ilea@ft.com>
2016-07-28 10:40:13 +0300

    Using the new pipeline for v1 annotations

--- reannotate.sh
+++ reannotate.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 while IFS= read -r line; do
-  curl "https://prod-us-up.ft.com/__nativerw/v1-metadata/$line" | curl -i -d @- -X PUT -H "Content-Type: application/json" "https://prod-us-up.ft.com/__up-queue-sender-v1-metadata/message/$line"
+  data=`curl -qSfs "https://upp-publishing-prod:xxx@pub-prod-uk-up.ft.com/__nativerw/v1-metadata/$line"`
+  if [ "$?" -eq 0 ]; then
+  echo -n $data | curl -i -d @- -X POST -H "Content-Type: application/json" -H"X-Origin-System-Id: binding-service" "https://upp-publishing-prod:xxx@pub-prod-uk-up.ft.com/metadata"
+  fi
 done
