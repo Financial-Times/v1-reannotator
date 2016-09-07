1f0cf872ffbeabe897a5cce0871749c66af4f617
Martin Garton <martin.garton@ft.com>
2016-09-07 10:34:18 +0100

    Discourage committing credentials
    Replace dummy user & password values with environment variables, to
    encourage users to set those variables locally instead of modifying the
    scripts directly.

--- reannotate-from-source.sh
+++ reannotate-from-source.sh
@@ -3,12 +3,12 @@ sources=`cat sources`
 while IFS= read -r line; do
   echo "Processing $line"
   for source in $sources; do
-	  status=`curl -qsL -w "%{http_code}" -H"ClientUserPrincipal: app-preditor" --digest -u "xxx:xxx" "http://metadata.internal.ft.com/metadata-services/binding/1.0/sources/$source/references/$line?includeTermAttrs=true&inflate=MAX" -o /dev/null 2>/dev/null`
+	  status=`curl -qsL -w "%{http_code}" -H"ClientUserPrincipal: app-preditor" --digest -u "$QMI_USER:$QMI_PASSWORD" "http://metadata.internal.ft.com/metadata-services/binding/1.0/sources/$source/references/$line?includeTermAttrs=true&inflate=MAX" -o /dev/null 2>/dev/null`
 	  if [ "$status" == "200" ]; then
 		  echo "$line found in source $source"
-		  data=`curl -qSfsL -H"ClientUserPrincipal: app-preditor" --digest -u "xxx:xxx" "http://metadata.internal.ft.com/metadata-services/binding/1.0/sources/$source/references/$line?includeTermAttrs=true&inflate=MAX" 2>/dev/null`
+		  data=`curl -qSfsL -H"ClientUserPrincipal: app-preditor" --digest -u "$QMI_USER:$QMI_PASSWORD" "http://metadata.internal.ft.com/metadata-services/binding/1.0/sources/$source/references/$line?includeTermAttrs=true&inflate=MAX" 2>/dev/null`
 		  #echo "$data"
-		  echo -n $data | go run json_envelope.go --uuid=$line | curl -i -d @- -X POST -H "Content-Type: application/json" -H"X-Origin-System-Id: binding-service" "https://upp-publishing-prod:xxx@pub-prod-uk-up.ft.com/metadata"
+		  echo -n $data | go run json_envelope.go --uuid=$line | curl -i -d @- -X POST -H "Content-Type: application/json" -H"X-Origin-System-Id: binding-service" "https://$UPP_USER:$UPP_PASSWORD@pub-prod-uk-up.ft.com/metadata"
 		  break
 	  fi
   done
