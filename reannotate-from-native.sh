62d4704db8db6a62abadf76035ef164e35b63146
Sandor Torok <sandor.torok@ft.com>
2016-09-12 15:39:43 +0300

    Added more logging

--- reannotate-from-native.sh
+++ reannotate-from-native.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
+echo "Started reannotate v1-metadata from Native Store"
 while IFS= read -r line; do
+  echo "Processing $line"
   data=`curl -qSfs "https://$UPP_USER:$UPP_PASSWORD@$UPP_HOST/__nativerw/v1-metadata/$line"`
   if [ "$?" -eq 0 ]; then
+  echo "Found $line in Native Store. Republishing..."
   echo -n $data | curl -i -d @- -X POST -H "Content-Type: application/json" -H"X-Origin-System-Id: binding-service" "https://$UPP_USER:$UPP_PASSWORD@$UPP_HOST/metadata"
   fi
 done
+echo "Finished"
