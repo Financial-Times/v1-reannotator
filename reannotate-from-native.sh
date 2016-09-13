974f653caea24941c62b7667eb9e684ea0464c37
Sandor Torok <sandor.torok@ft.com>
2016-09-13 15:27:11 +0300

    For loop

--- reannotate-from-native.sh
+++ reannotate-from-native.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-if [ -z "$UPP_USER" ] || [ -z "$UPP_PASSWORD" ] || [ -z "$UPP_HOST" ]; then
+if [ -z "$UPP_USER" ] || [ -z "$UPP_PASSWORD" ] || [ -z "$UPP_HOST" ] || [ -z "$uuidList" ]; then
   echo "One of the required environment variables are not set up. Please correct"
   exit 2
 fi
 
 echo "Started reannotate v1-metadata from Native Store"
-while IFS= read -r line; do
-  echo "Processing $line"
-  data=`curl -qSfs "https://$UPP_USER:$UPP_PASSWORD@$UPP_HOST/__nativerw/v1-metadata/$line"`
+for uuid in ${uuidList}; do
+  echo "Processing $uuid"
+  data=`curl -qSfs "https://$UPP_USER:$UPP_PASSWORD@$UPP_HOST/__nativerw/v1-metadata/$uuid"`
   if [ "$?" -eq 0 ]; then
-    echo "Found $line in Native Store. Republishing..."
+    echo "Found $uuid in Native Store. Republishing..."
     echo -n $data | curl -i -d @- -X POST -H "Content-Type: application/json" -H"X-Origin-System-Id: binding-service" "https://$UPP_USER:$UPP_PASSWORD@$UPP_HOST/metadata"
   else
-    echo ">$line not found in Native Store. Data: $data"
+    echo ">$uuid not found in Native Store. Data: $data"
   fi
 done
 echo "Finished"
