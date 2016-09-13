c86c3fb0b225089d3cdb558165d3b2f6395eb60d
Sandor Torok <sandor.torok@ft.com>
2016-09-13 11:42:48 +0300

    More logging

--- reannotate-from-native.sh
+++ reannotate-from-native.sh
@@ -4,8 +4,10 @@ while IFS= read -r line; do
   echo "Processing $line"
   data=`curl -qSfs "https://$UPP_USER:$UPP_PASSWORD@$UPP_HOST/__nativerw/v1-metadata/$line"`
   if [ "$?" -eq 0 ]; then
-  echo "Found $line in Native Store. Republishing..."
-  echo -n $data | curl -i -d @- -X POST -H "Content-Type: application/json" -H"X-Origin-System-Id: binding-service" "https://$UPP_USER:$UPP_PASSWORD@$UPP_HOST/metadata"
+    echo "Found $line in Native Store. Republishing..."
+    echo -n $data | curl -i -d @- -X POST -H "Content-Type: application/json" -H"X-Origin-System-Id: binding-service" "https://$UPP_USER:$UPP_PASSWORD@$UPP_HOST/metadata"
+  else
+    echo ">$line not found in Native Store"
   fi
 done
 echo "Finished"
--- reannotate-from-source.sh
+++ reannotate-from-source.sh
@@ -10,6 +10,8 @@ while IFS= read -r line; do
 		  data=`curl -qSfsL -H"ClientUserPrincipal: app-preditor" --digest -u "$QMI_USER:$QMI_PASSWORD" "http://metadata.internal.ft.com/metadata-services/binding/1.0/sources/$source/references/$line?includeTermAttrs=true&inflate=MAX" 2>/dev/null`
 		  echo -n $data | go run json_envelope.go --uuid=$line | curl -i -d @- -X POST -H "Content-Type: application/json" -H"X-Origin-System-Id: binding-service" "https://$UPP_USER:$UPP_PASSWORD@$UPP_HOST/metadata"
 		  break
+	  else
+	    echo ">$line not found in Binding Service"
 	  fi
   done
 done
