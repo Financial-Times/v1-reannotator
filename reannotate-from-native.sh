23de5f1bb485b7bf341f61b3feca9012aa15592d
Sandor Torok <sandor.torok@ft.com>
2016-09-13 15:31:13 +0300

    More debug

--- reannotate-from-native.sh
+++ reannotate-from-native.sh
@@ -8,12 +8,13 @@ fi
 echo "Started reannotate v1-metadata from Native Store"
 for uuid in ${uuidList}; do
   echo "Processing $uuid"
+  debug=`curl -i "https://$UPP_USER:$UPP_PASSWORD@$UPP_HOST/__nativerw/v1-metadata/$uuid"`
   data=`curl -qSfs "https://$UPP_USER:$UPP_PASSWORD@$UPP_HOST/__nativerw/v1-metadata/$uuid"`
   if [ "$?" -eq 0 ]; then
     echo "Found $uuid in Native Store. Republishing..."
     echo -n $data | curl -i -d @- -X POST -H "Content-Type: application/json" -H"X-Origin-System-Id: binding-service" "https://$UPP_USER:$UPP_PASSWORD@$UPP_HOST/metadata"
   else
-    echo ">$uuid not found in Native Store. Data: $data"
+    echo ">$uuid not found in Native Store. Data: $debug"
   fi
 done
 echo "Finished"
