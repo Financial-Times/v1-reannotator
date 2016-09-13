d2988173d1337c964e4700e04abbb6d91af91dff
Sandor Torok <sandor.torok@ft.com>
2016-09-13 15:39:55 +0300

    More logging and for loop

--- reannotate-from-native.sh
+++ reannotate-from-native.sh
@@ -8,13 +8,13 @@ fi
 echo "Started reannotate v1-metadata from Native Store"
 for uuid in ${uuidList}; do
   echo "Processing $uuid"
-  debug=`curl -i "https://$UPP_USER:$UPP_PASSWORD@$UPP_HOST/__nativerw/v1-metadata/$uuid"`
-  data=`curl -qSfs "https://$UPP_USER:$UPP_PASSWORD@$UPP_HOST/__nativerw/v1-metadata/$uuid"`
-  if [ "$?" -eq 0 ]; then
+  status=`url -qsL -w "%{http_code}" "https://$UPP_USER:$UPP_PASSWORD@$UPP_HOST/__nativerw/v1-metadata/$uuid" -o /dev/null 2>/dev/null`
+  if [ "$status" == "200" ]; then
+    data=`curl -qSfs "https://$UPP_USER:$UPP_PASSWORD@$UPP_HOST/__nativerw/v1-metadata/$uuid"`
     echo "Found $uuid in Native Store. Republishing..."
     echo -n $data | curl -i -d @- -X POST -H "Content-Type: application/json" -H"X-Origin-System-Id: binding-service" "https://$UPP_USER:$UPP_PASSWORD@$UPP_HOST/metadata"
   else
-    echo ">$uuid not found in Native Store. Data: $debug"
+    echo ">$uuid cannot be obtained from Native Store. Status: $status"
   fi
 done
 echo "Finished"
--- reannotate-from-source.sh
+++ reannotate-from-source.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 echo "Started reannotate v1-metadata from Binding Service"
 sources=`cat sources`
-while IFS= read -r line; do
-  echo "Processing $line"
+for uuid in ${uuidList}; do
+  echo "Processing $uuid"
   for source in $sources; do
-	  status=`curl -qsL -w "%{http_code}" -H"ClientUserPrincipal: app-preditor" --digest -u "$QMI_USER:$QMI_PASSWORD" "http://metadata.internal.ft.com/metadata-services/binding/1.0/sources/$source/references/$line?includeTermAttrs=true&inflate=MAX" -o /dev/null 2>/dev/null`
+	  status=`curl -qsL -w "%{http_code}" -H"ClientUserPrincipal: app-preditor" --digest -u "$QMI_USER:$QMI_PASSWORD" "http://metadata.internal.ft.com/metadata-services/binding/1.0/sources/$source/references/$uuid?includeTermAttrs=true&inflate=MAX" -o /dev/null 2>/dev/null`
 	  if [ "$status" == "200" ]; then
-		  echo "$line found in source $source"
-		  data=`curl -qSfsL -H"ClientUserPrincipal: app-preditor" --digest -u "$QMI_USER:$QMI_PASSWORD" "http://metadata.internal.ft.com/metadata-services/binding/1.0/sources/$source/references/$line?includeTermAttrs=true&inflate=MAX" 2>/dev/null`
-		  echo -n $data | go run json_envelope.go --uuid=$line | curl -i -d @- -X POST -H "Content-Type: application/json" -H"X-Origin-System-Id: binding-service" "https://$UPP_USER:$UPP_PASSWORD@$UPP_HOST/metadata"
+		  echo "$uuid found in source $source"
+		  data=`curl -qSfsL -H"ClientUserPrincipal: app-preditor" --digest -u "$QMI_USER:$QMI_PASSWORD" "http://metadata.internal.ft.com/metadata-services/binding/1.0/sources/$source/references/$uuid?includeTermAttrs=true&inflate=MAX" 2>/dev/null`
+		  echo -n $data | go run json_envelope.go --uuid=$uuid | curl -i -d @- -X POST -H "Content-Type: application/json" -H"X-Origin-System-Id: binding-service" "https://$UPP_USER:$UPP_PASSWORD@$UPP_HOST/metadata"
 		  break
 	  else
-	    echo ">$line not found in Binding Service"
+	    echo ">$uuid cannot be obtained from Binding Service. Status: $status"
 	  fi
   done
 done
